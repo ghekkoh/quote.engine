ded8477ba742325f96581778b3192ca7dc59cacc
Ryan Doto <d.ryan.doto@gmail.com>
2017-07-21 14:41:49 +0000

    first test, good job

--- tests/Feature/RandomTest.php
+++ tests/Feature/RandomTest.php
@@ -7,17 +7,19 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class RandomTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testDisplayRandomQuote()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        
+        $response->assertSee('my favorite quote');
     }
 }
